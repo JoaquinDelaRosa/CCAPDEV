0bf772a821a2a030f86d070978d3939975914ffa
JoaquinDelaRosa <joaquin_delarosa@dlsu.edu.ph>
2022-04-09 07:44:11 +0800

    add: comments to postpage

--- src/pages/comment.jsx
+++ src/pages/comment.jsx
@@ -1,11 +1,64 @@
-import React, { useState } from "react";
+import React from "react";
 
-function Comment(comment){
+function Comment({ post }) {
   return (
-    <div className="">
-      This is a comment
+    <div className="p-0 w-fit">
+      <div id="comment-box" className="w-full h-auto">
+        <div id="comment-author" className="h-fit mb-1 px-0">
+          <p className="text-left font-mono text-lg font-semibold">
+            {post.author}
+          </p>
+        </div>
+
+        {/* Note that comments do not require images */}
+        {(post.mediaPath !== null) ?
+          <div id="comment-media" className="mb-1 h-fit w-full flex">
+            <img src={post.mediaPath} alt={post.mediaAlt}
+            className="max-w-[20rem] max-h-[20rem]"
+            />
+          </div>
+          : null
+        }
+
+        <div id="post-body-section" className="h-fit pl-5">
+          <p className="font-mono text-justify">
+            {post.body}
+          </p>
+        </div>
+
+        <div id="rating-bar" className="flex h-fit mb-2">
+          <div className="w-fit mr-3">
+            <input type="button"
+              className="text-sm font-semibold text-gray-400 hover:text-blue-900 hover:cursor-pointer"
+              defaultValue={"Show Comments"}
+            />
+          </div>
+
+          <div className="align-middle">
+            <p className="w-fit mr-3 font-extrabold text-green-400"> {post.upvotes} </p>
+          </div>
+
+          <div>
+            <p className="w-fit mr-3 font-extrabold text-red-400"> {post.downvotes} </p>
+          </div>
+        </div>
+
+        <div id="comment-section" className=" w-full h-auto " >
+        {
+          post.comments.map( (value) => {
+              return (
+                <div className="flex">
+                  <div className="w-2 h-full"> </div>
+                  <Comment post={value} key={value.id}/>
+                </div>
+              )
+            })
+        }
+        </div>
+
+      </div>
     </div>
-  )
+    )
 }
 
 export default Comment;
--- src/pages/postpage.jsx
+++ src/pages/postpage.jsx
@@ -3,23 +3,22 @@ import React, { useState } from "react";
 import Comment from "./comment";
 
 function PostPage(){
-  const [post, createPost] = useState({
+  const [post, ] = useState({
     "id": "",
-    "postTitle": "",
-    "postMediaPath" : "",
-    "postBody" : "",
+    "title": "",
+    "mediaPath" : "",
+    "body" : "",
     "upvotes"  : "",
     "downvotes" : "",
     "views"     : "",
     "author"    : "",
-    "comments" : {
-    }
+    "comments" : []
   });
 
   // Placeholder values
-  post.postTitle = "Introducing The turboencabulator";
-  post.author = "John Doe"
-  post.postBody = 
+  post.title = "Introducing The turboencabulator";
+  post.author = "Anonymous"
+  post.body = 
   " The original machine has a base-plate of prefabulated amulite, surmounted by a malleable logarithmic \
     casing in such a way that the two spurving bearings were in a direct line with the pentametric fan. \
     The latter consisted simply of six hydrocoptic marzelvanes, so fitted to the ambifacient lunar waneshaft \
@@ -29,51 +28,97 @@ function PostPage(){
   post.upvotes = 10;
   post.downvotes = 20;
   post.views = 100;
-  post.postMediaPath = require("../images/sample.png");  
+  post.mediaPath = require("../images/sample.png"); 
+
+  post.comments.push({
+    "id": "1",
+    "mediaPath" : null,
+    "mediaAlt" : "",
+    "body" : "Wow! Great!",
+    "upvotes"  : "1",
+    "downvotes" : "10",
+    "views"     : "20",
+    "author"    : "Jane Doe",
+    "comments" : []
+  })
+
+  post.comments.push({
+    "id": "2",
+    "mediaPath" : null,
+    "mediaAlt" : "",
+    "body" :   " The original machine has a base-plate of prefabulated amulite, surmounted by a malleable logarithmic \
+    casing in such a way that the two spurving bearings were in a direct line with the pentametric fan. \
+    The latter consisted simply of six hydrocoptic marzelvanes, so fitted to the ambifacient lunar waneshaft \
+    that side fumbling was effectively prevented. The main winding was of the normal lotus-o-delta type placed \
+    in panendermic semiboloid slots in the stator, every seventh conductor being connected by a non-reversible \
+    tremie pipe to the differential girdlespring on the \"up\" end of the grammeters.",
+    "upvotes"  : "1",
+    "downvotes" : "10",
+    "views"     : "20",
+    "author"    : "Jane Doe",
+    "comments" : []
+  })
+
+  post.comments[0].comments.push({
+    "id": "2",
+    "mediaPath" : null,
+    "mediaAlt" : "",
+    "body" :   " The original machine has a base-plate of prefabulated amulite, surmounted by a malleable logarithmic \
+    casing in such a way that the two spurving bearings were in a direct line with the pentametric fan. \
+    The latter consisted simply of six hydrocoptic marzelvanes, so fitted to the ambifacient lunar waneshaft \
+    that side fumbling was effectively prevented. The main winding was of the normal lotus-o-delta type placed \
+    in panendermic semiboloid slots in the stator, every seventh conductor being connected by a non-reversible \
+    tremie pipe to the differential girdlespring on the \"up\" end of the grammeters.",
+    "upvotes"  : "1",
+    "downvotes" : "10",
+    "views"     : "20",
+    "author"    : "Jane Doe",
+    "comments" : []
+  })
 
   return (
       <div className="mt-2">
         <div id="post-section" className= "w-full h-auto">
           <div id="post-title-section" className="px-10 h-fit pt-5 pb-3">
             <strong className="text-left font-extrabold font-sans text-4xl align-baseline tracking-wider"> 
-              {post.postTitle}
+              {post.title}
             </strong>
           </div>
 
           <div id="post-author-section" className="px-10 h-fit pb-5">
-            <text className="text-left font-mono "> 
+            <p className="text-left font-mono "> 
               By: {post.author}
-            </text>
+            </p>
           </div>
 
           <div id="post-media-section" className="px-10 h-fit w-full pb-5 flex justify-center">
-            <img src={post.postMediaPath} alt="A sample" className="self-center max-w-[40rem] max-h-[40rem]"/>
+            <img src={post.mediaPath} alt={post.mediaAlt} className="self-center max-w-[40rem] max-h-[40rem]"/>
           </div>
 
           <div id="post-body-section" className="px-10 h-fit pb-5">
             <p className="font-mono text-justify">
-              {post.postBody}
+              {post.body}
               </p>
             </div>
         </div>
         <div id="rating-bar" className="px-10 flex h-fit"> 
           <div className="w-full">
             <input type="button" 
-            className="text-l font-semibold text-gray-200 hover:text-blue-900 hover:cursor-pointer" 
+            className="text-l font-semibold text-gray-400 hover:text-blue-900 hover:cursor-pointer" 
             defaultValue={"Show Comments"}
             /> 
           </div>
 
           <div>
-            <p className="mr-5 text-xl font-extrabold text-green-400"> {post.upvotes} </p>
+            <p className="w-fit mr-5 text-xl font-extrabold text-green-400"> {post.upvotes} </p>
           </div>
 
           <div>
-            <p className="mr-5 text-xl font-extrabold text-red-400"> {post.downvotes} </p>
+            <p className="w-fit mr-5 text-xl font-extrabold text-red-400"> {post.downvotes} </p>
           </div>
 
-          <div className="text-right text-xl  text-gray-400">
-            <p className="font-bold"> {post.views} &nbsp; </p>
+          <div className="w-fit text-left text-xl mr-1 text-gray-400">
+            <p className="font-bold"> {post.views}</p>
           </div>
 
           <div className="w-fit text-right text-xl  text-gray-400">
@@ -81,9 +126,21 @@ function PostPage(){
           </div>
         </div>
 
+        <div id="comment-section" className=" w-fit h-auto mx-10" >
+          {
+            post.comments.map( (value) => {
+              return (
+                <div className="flex border-4 pr-4 py-2">
+                  <div className="w-5 h-full "> </div>
+                  <Comment post={value} key={value.id}/>
+                </div>
+              )
+            })
+          }
+        </div>
 
-        <div id="comment-section" className=" w-full h-auto px-10" >
-          <Comment  ></Comment>
+        <div id="footer" className="h-[3rem]">
+
         </div>
       </div>
   )
