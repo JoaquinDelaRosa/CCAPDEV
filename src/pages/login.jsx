3eb8cfbccd88d133f987c1179121191fdcef9025
JoaquinDelaRosa <joaquin_delarosa@dlsu.edu.ph>
2022-04-12 15:20:32 +0800

    style: clean up login

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -34,66 +34,68 @@ function Login(){
 
 
     return (
-        <div className="flex justify-center items-center h-screen">
-          <div className="flex max-w-fit">
-            <form>
-              <span className="flex justify-center items-center">
-                <h2 className="m-0.5  text-center font-sans font-extrabold w-80 "> Login into your account </h2>
-              </span>
-              <div className="m-2">
-                <input type="text" id="reg-username" className="form-input bg-gray-100 text-left font-sans font-light w-80"
-                  value={login["username"]}
-                  onChange={(e) => 
-                    inputHandler("username", e.target.value)
-                  }
-                  onFocus={(e) =>{
-                    if(e.target.value === defaultUserName)
-                      inputHandler("username", "");
-                  }}
-                  onBlur={(e)=>{
-                    if(login["username"] === "")
-                      inputHandler("username", defaultUserName);
-                  }}
-                  required
+      <div className="flex justify-center items-center h-screen">
+        <div className="flex max-w-fit">
+          <form>
+            <span className="flex justify-center items-center">
+              <h2 className="m-0.5  text-center font-sans font-extrabold w-80 "> Login into your account </h2>
+            </span>
+            
+            <div className="m-2">
+              <input type="text" id="reg-username" className="form-input bg-gray-100 text-left font-sans font-light w-80"
+                value={login["username"]}
+                onChange={(e) => 
+                  inputHandler("username", e.target.value)
+                }
+                onFocus={(e) =>{
+                  if(e.target.value === defaultUserName)
+                    inputHandler("username", "");
+                }}
+                onBlur={(e)=>{
+                  if(login["username"] === "")
+                    inputHandler("username", defaultUserName);
+                }}
+                required
                 />
               </div>
 
-              <div className="m-2">
-                <input type={passwordInputType} id="reg-password" className="form-input bg-gray-100 text-left font-sans font-light w-80"
-                  value={login["password"]}
-                  onChange={(e)=> { 
-                    inputHandler("password", e.target.value)
-                  }}
-                  onFocus = {(e) => {
-                    if(e.target.value === defaultPassword){
-                      setPasswordInputType("password");
-                      inputHandler("password", "");
-                    }
-                  }}
-                  onBlur={(e)=>{
-                    if(login["password"] === ""){
-                      inputHandler("password", defaultPassword);
-                      setPasswordInputType("text");
-                    }
-                  }}
-                  required
-                  />
-              </div>
+            <div className="m-2">
+              <input type={passwordInputType} id="reg-password" className="form-input bg-gray-100 text-left font-sans font-light w-80"
+                value={login["password"]}
+                onChange={(e)=> { 
+                  inputHandler("password", e.target.value)
+                }}
+                onFocus = {(e) => {
+                  if(e.target.value === defaultPassword){
+                    setPasswordInputType("password");
+                    inputHandler("password", "");
+                  }
+                }}
+                onBlur={(e)=>{
+                  if(login["password"] === ""){
+                    inputHandler("password", defaultPassword);
+                    setPasswordInputType("text");
+                  }
+                }}
+                required
+              />
+            </div>
 
-              <span className="flex justify-center items-center">
-                  <input type="submit" value="Create Account" disabled={isSubmitDisabled()}
-                  className= {"border-2 rounded-full w-auto text-white " + 
-                  (isSubmitDisabled() ? "bg-blue-200" : "bg-green-500 hover:cursor-pointer")}
-                  />
-              </span>
-              <br/>
-              <p className="flex justify-center items-center font-mono"> 
-                Don't have an account? &nbsp; <Link to="../registration" className="text-blue-300 hover:text-blue-600"> Register now! </Link>
-              </p>
-            </form>
+            <span className="flex justify-center items-center">
+              <input type="submit" value="Create Account" disabled={isSubmitDisabled()}
+                className= {"border-2 rounded-full w-auto text-white " + 
+                (isSubmitDisabled() ? "bg-blue-200" : "bg-green-500 hover:cursor-pointer")}
+              />
+            </span>
+            
+            <br/>
+            <p className="flex justify-center items-center font-mono"> 
+              Don't have an account? &nbsp; <Link to="../registration" className="text-blue-300 hover:text-blue-600"> Register now! </Link>
+            </p>
+          </form>
 
-          </div> 
-        </div>
+        </div> 
+      </div>
     )
 }
 
