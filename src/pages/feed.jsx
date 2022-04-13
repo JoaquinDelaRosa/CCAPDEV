90fa8af42bb414bddb594080477aad10628e22ff
JoaquinDelaRosa <joaquin_delarosa@dlsu.edu.ph>
2022-04-14 05:40:28 +0800

    refactor: reorganize feed

---
+++ src/pages/feed.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+function Feed(){
+  return (
+    <div>
+      {/* Webpage Header */}
+      <div>
+        <h2 className="font-mono font-extrabold"> Foo Bar</h2>
+      </div>
+
+      <div> 
+        {
+        /* Feed itself. This will contain all the posts. Structure the posts in JSON. Refer to postpage for an example. No need to 
+        include comments.
+        */ 
+        }
+      </div>
+
+      <div>
+        {
+        /* Side bar. THis is where you put, say, popular posts.
+        */
+        }
+      </div>
+
+
+      <div>
+        {/* Footer */}
+      </div>
+    </div>
+  )
+}
+
+export default Feed;
