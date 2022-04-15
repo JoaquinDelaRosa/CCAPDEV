105f6941be4db6363865dd22450b0ad238d9ab2c
Shaun <ongshaun0902@gmail.com>
2022-04-15 20:37:44 +0800

    add: postbox

---
+++ src/pages/postbox.jsx
@@ -0,0 +1,29 @@
+﻿import React from "react";
+
+function Postbox() {
+    return (
+        <div>
+            <div>
+                <p>Title</p>
+            </div>
+
+            <div>
+                <p>Author</p>
+            </div>
+
+            <div>
+                <p>Date</p>
+            </div>
+
+            <div>
+                <p>Image</p>
+            </div>
+
+            <div>
+                <p>Statistics</p>
+            </div>
+        </div>
+    )
+}
+
+export default Postbox;
