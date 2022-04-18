47899b54294fa821637122e196204b5c8689055c
Shaun <ongshaun0902@gmail.com>
2022-04-18 23:25:51 +0800

    Update Feed and Postbox
    Not much done since last time. Will work on it more.

--- src/pages/feed.jsx
+++ src/pages/feed.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Postbox from "./postbox";
 
-function Feed(){
+const p = {
+    "id": 1,
+    "title": "Introducing The turboencabulator",
+    "author": "Anonymous",
+    "date": new Date(),
+    "mediaPath": require("../images/sample.png"),
+    "mediaAlt": "",
+    "upvotes": 10,
+    "downvotes": 20,
+    "views": 100,
+    }
+ 
+
+
+function Feed() {
+
+    const [post, setPost] = useState({
+        "id": "",
+        "title": "",
+        "author": "",
+        "date": new Date(),
+        "mediaPath": null,
+        "mediaAlt": "",
+        "upvotes": 0,
+        "downvotes": 0,
+        "views": 0,
+    });
+
+    useEffect(
+        () => { setPost(p) }, []
+    );
+
   return (
     <div>
       {/* Webpage Header */}
@@ -10,11 +41,16 @@ function Feed(){
       </div>
 
           <div>
-        {
-        /* Feed itself. This will contain all the posts. Structure the posts in JSON. Refer to postpage for an example. No need to 
-        include comments.
-        */
-                  <Postbox/>
+              {
+                  /* Feed itself. This will contain all the posts. Structure the posts in JSON. Refer to postpage for an example. No need to 
+                  include comments.
+                  */
+                  
+                  [p].map(element => { 
+                      return <Postbox content={content}/>
+                  })
+                          
+                  
         }
       </div>
 
--- src/pages/postbox.jsx
+++ src/pages/postbox.jsx
@@ -1,26 +1,50 @@
-﻿import React from "react";
+﻿import React, { useEffect, useState } from "react";
 
-function Postbox() {
+function Postbox({ content }) {
+    const [post, setPost] = useState({
+        "id": "",
+        "title": "",
+        "author": "",
+        "date": new Date(),
+        "mediaPath": null,
+        "mediaAlt": "",
+        "upvotes": 0,
+        "downvotes": 0,
+        "views": 0,
+    });
+
+    useEffect(() => {
+        if (content !== undefined)
+            setPost(content);
+    }, [content]);
+
     return (
         <div>
             <div>
                 <p>Title</p>
+                {post.title}
             </div>
 
             <div>
                 <p>Author</p>
+                {post.author}
             </div>
 
             <div>
                 <p>Date</p>
+                {post.date}
             </div>
 
             <div>
                 <p>Image</p>
+                {post.mediaPath}
             </div>
 
             <div>
                 <p>Statistics</p>
+                {post.upvotes}
+                {post.downvotes}
+                {post.views}
             </div>
         </div>
     )
