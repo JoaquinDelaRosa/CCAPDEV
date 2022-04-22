0e0ca47eb0e571a6947fc934e2a517a9da06954f
JoaquinDelaRosa <joaquin_delarosa@dlsu.edu.ph>
2022-04-23 07:18:20 +0800

    chore: code style

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -1,79 +1,69 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
 import parseDate from "../utils/date";
 
 
 function Profile({profileData}){
-    const [profile, setProfile] = useState({
-        "id" : 0,
-        "pfp": null,
-        "name": "",
-        "username": "",
-        "password": "",
-        "about": "",
-        "gender": "",
-        "saves": [],
-        "posts" : [],
-        "dateJoined": new Date()
-    });
+  const [profile, setProfile] = useState({
+    "id" : 0,
+    "pfp": null,
+    "name": "",
+    "username": "",
+    "password": "",
+    "about": "",
+    "gender": "",
+    "saves": [],
+    "posts" : [],
+    "dateJoined": new Date()
+  });
     
-    useEffect(
-        () => {setProfile(profileData)}, [profileData]
-      )
+  useEffect(
+    () => {setProfile(profileData)}, [profileData]
+  )
 
-    const [pfp, setpfp] = useState(false);
-    const [abouting, setAbout] = useState(false);
-    const [saving, setSave] = useState(false);
-
-    const handleSettings = () => {
-        // make all editable information in the info-section editable textbox. make save changes button.
-    
-    }
-
-    return (
-        <div className="flex flex-auto h-screen p-4 bg-gray-800 text-white" id="main">
-            <div className="w-full min-w-3/4 h-fit p-2 justify-start" id="left-box">
-                <div className="p-2" id="saves-section">
-                    <button className="text-cyan-500 font-mono rounded-lg border-white border-2 px-1 py-0.5">
-                        Saves
-                    </button>
-                </div>
-            </div>
-            
-            <div className="w-fit h-fit p-2 mr-4 flex justify-start bg-gray-700 rounded-lg" id="right-box">
-                <div className="flex justify-right align-middle  w-fit h-fit" id="pfp-section">
-                    <img src={profile["pfp"]} 
-                        alt={profile["username"] + "'s profile picture"}
-                        className = "rounded-full"
-                        width={128}
-                        height={128}
-                    />
-                </div>
-
-                <div className="w-fit ml-4" id="info-section">
-                    <span className="mb-2" id="username-section">
-                        <h3> {profile["username"]} </h3>
-                    </span>
-
-                    <span className="mb-2" id="about-section">
-                        <p> <strong> About Me: </strong> </p>
-                        <p> {profile["about"]} </p>
-                    </span>
-
-                    <div className="mb-2" id="other-section">
-                        <div className="" id="gender-section">
-                            <p> {profile["gender"]} </p>
-                        </div>
-
-                        <div className="" id="datejoined-section">
-                            <p> <strong> User since: </strong> </p>
-                            <p> {parseDate(profile["dateJoined"])} </p> 
-                        </div>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="flex flex-auto h-screen p-4 bg-gray-800 text-white" id="main">
+      <div className="w-full min-w-3/4 h-fit p-2 justify-start" id="left-box">
+        <div className="p-2" id="saves-section">
+          <button className="text-cyan-500 font-mono rounded-lg border-white border-2 px-1 py-0.5">
+            Saves
+           </button>
         </div>
-    )
+      </div>
+        
+      <div className="w-fit h-fit p-2 mr-4 flex justify-start bg-gray-700 rounded-lg" id="right-box">
+        <div className="flex justify-right align-middle  w-fit h-fit" id="pfp-section">
+          <img src={profile["pfp"]} 
+            alt={profile["username"] + "'s profile picture"}
+            className = "rounded-full"
+            width={128}
+            height={128}
+          />
+        </div>
+
+        <div className="w-fit ml-4" id="info-section">
+          <span className="mb-2" id="username-section">
+            <h3> {profile["username"]} </h3>
+          </span>
+
+          <span className="mb-2" id="about-section">
+            <p> <strong> About Me: </strong> </p>
+            <p> {profile["about"]} </p>
+          </span>
+
+          <div className="mb-2" id="other-section">
+            <div className="" id="gender-section">
+              <p> {profile["gender"]} </p>
+            </div>
+
+            <div className="" id="datejoined-section">
+              <p> <strong> User since: </strong> </p>
+              <p> {parseDate(profile["dateJoined"])} </p> 
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Profile;
--- src/pages/settingspage.jsx
+++ src/pages/settingspage.jsx
@@ -1,97 +1,113 @@
 import React, { useEffect, useState } from "react";
 
 function SettingsPage({profile, setProfile}){
-    const [editted, setEditted] = useState({
-        "id" : 0,
-        "pfp": null,
-        "name": "",
-        "username": "",
-        "password" : "",
-        "about": "",
-        "gender": "",
-        "saves": [],
-        "posts" : [],
-        "dateJoined": new Date()
-    });
+  const [editted, setEditted] = useState({
+    "id" : 0,
+    "pfp": null,
+    "name": "",
+    "username": "",
+    "password" : "",
+    "about": "",
+    "gender": "",
+    "saves": [],
+    "posts" : [],
+    "dateJoined": new Date()
+  });
 
-    useEffect(() => {
-        setProfile(profile);
-    }, [profile, setProfile]);
+  useEffect(() => {
+    setProfile(profile);
+  }, [profile, setProfile]);
 
-    useEffect(() => {
-        if(profile !== null){
-            setEditted(profile);
-        }
-    }, [setEditted, profile]);
+  useEffect(() => {
+    if(profile !== null){
+      setEditted(profile);
+    }
+  }, [setEditted, profile]);
 
     
-    const inputHandler = (name, value) => {
-        setEditted( values => ({...values, [name] : value}))
-    }
+  const inputHandler = (name, value) => {
+    setEditted( values => ({...values, [name] : value}))
+  }
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        Object.assign(profile, editted);
-        setProfile(profile);
-        setEditted(profile);
-    }
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    Object.assign(profile, editted);
+    setProfile(profile);
+    setEditted(profile);
+  }
 
-    return (
-        <div>
-            <div id="edit section">
-                Edit your profile!
-                <form onSubmit= {handleSubmit}>
-                    <div className="flex" id="username">
-                        <div> <h2 className="mr-3"> Name: </h2> </div>
-                        <div> 
-                            <input type="text"
-                                placeholder={editted["username"]}
-                                onChange={(e) => {inputHandler("username", e.target.value)}}
-                            />
-                        </div>
-                    </div>
+  return (
+    <div>
+      <div id="edit section">
+        <h1> 
+          Edit your profile!
+        </h1>
 
-                    <div className="flex" id="password">
-                        <div> <h2 className="mr-3"> Password: </h2> </div>
-                        <div> 
-                            <input type="password"
-                                placeholder={"Change password"}
-                                onChange={(e) => {inputHandler("password", e.target.value)}}
-                            />
-                        </div>
-                    </div>
-
-                    <div id="about-me">
-                        <div> <h2 className="mr-3"> About Me: </h2> </div>
-                        <div> 
-                        <textarea
-                            className="w-full h-fit mb-1 text-lg font-mono bg-slate-50 overflow-hidden resize-none"
-                            placeholder={profile["about"]}
-                            onChange={(e) => {
-                                inputHandler("about", e.target.value);
-                                e.target.style.height = 'inherit';
-                                e.target.style.height = `${e.target.scrollHeight}px`; 
-                            }}
-                        />
-                        </div>
-                    </div>
-
-                    <div id="submit">
-                        <input type="submit"
-                            value={"Save Changes"}
-                            className= {"py-1 px-8 rounded-full w-auto text-white bg-orange-500 hover:cursor-pointer hover:bg-orange-600"}
-                        />
-                    </div>
-                    
-                </form>
+        <form onSubmit= {handleSubmit}>
+          <div className="flex" id="username">
+            <div> 
+              <h2 className="mr-3"> Name: </h2> 
             </div>
 
-            <div id="delete section">
-                Delete your profile?
-                {/* Backend logic for profile deletion goes here */}
+            <div> 
+              <input type="text"
+                placeholder={editted["username"]}
+                onChange={(e) => {inputHandler("username", e.target.value)}}
+              />
             </div>
-        </div>
-    )
+          </div>
+
+          <div className="flex" id="password">
+            <div> 
+              <h2 className="mr-3">
+                Password: 
+              </h2> 
+            </div>
+
+            <div> 
+              <input type="password"
+                placeholder={"Change password"}
+                onChange={(e) => {inputHandler("password", e.target.value)}}
+              />
+            </div>
+          </div>
+
+          <div id="about-me">
+            <div> 
+              <h2 className="mr-3"> 
+              About Me: 
+              </h2> 
+            </div>
+
+            <div> 
+              <textarea
+                className="w-full h-fit mb-1 text-lg font-mono bg-slate-50 overflow-hidden resize-none"
+                placeholder={profile["about"]}
+                onChange={(e) => {
+                  inputHandler("about", e.target.value);
+                  e.target.style.height = 'inherit';
+                  e.target.style.height = `${e.target.scrollHeight}px`; 
+                }}
+              />
+            </div>
+          </div>
+
+          <div id="submit">
+            <input type="submit"
+              value={"Save Changes"}
+              className= {"py-1 px-8 rounded-full w-auto text-white bg-orange-500 hover:cursor-pointer hover:bg-orange-600"}
+            />
+          </div>
+                
+        </form>
+      </div>
+
+      <div id="delete section">
+         Delete your profile?
+            {/* Backend logic for profile deletion goes here */}
+      </div>
+    </div>
+  )
 }
 
 export default SettingsPage;
