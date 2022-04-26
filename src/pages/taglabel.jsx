a95edc19b1964ea52018d49a93626f1387fe869a
JoaquinDelaRosa <joaquin_delarosa@dlsu.edu.ph>
2022-04-26 13:55:11 +0800

    chore: code style

--- src/pages/taglabel.jsx
+++ src/pages/taglabel.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 
 function TagLabel({content}){
-    return (
-        <div>
-            <div className="bg-sky-200 py-0.5 px-3 w-fit mr-4 mb-2 rounded-full">
-                <p className="text-1xl font-bold"> 
-                    {content} 
-                </p>
-            </div>
-        </div>
-    )
+  return (
+    <div>
+      <div className="bg-sky-200 py-0.5 px-3 w-fit mr-4 mb-2 rounded-full">
+        <p className="text-1xl font-bold"> 
+          {content} 
+        </p>
+      </div>
+    </div>
+  )
 }
 
 export default TagLabel;
