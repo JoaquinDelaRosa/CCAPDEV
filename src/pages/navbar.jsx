1b32cea7fbfecc17a04af6f6eef2cb5498ce20d8
JoaquinDelaRosa <joaquin_delarosa@dlsu.edu.ph>
2022-04-26 20:13:20 +0800

    remove: hash as key in login

--- src/pages/navbar.jsx
+++ src/pages/navbar.jsx
@@ -1,15 +1,14 @@
 import React, { useEffect, useState }  from "react";
 import { Link } from "react-router-dom";
 
-const loggedInNavbar = [
+const loggedOutNavbar = [
   ['Home', './'],
-  ['Post', './postpage'],
   ['Feed', './feed'],
   ['Log in', './login'],
   ['Sign Up', './registration']
 ]
 
-const loggedOutNavbar = [ 
+const loggedInNavbar = [ 
   ['Home', './'],
   ['Post', './postpage'],
   ['Feed', './feed'],
@@ -30,14 +29,11 @@ function Navbar({profile, setProfile}){
   
   const [navbar, setNavbar] = useState(loggedInNavbar);
 
-  useEffect(() => {
-    setNavbar(navbar)
-  }, [navbar]);
   
   useEffect(() => {
       profile === null ? 
-        setNavbar(loggedInNavbar) : 
-        setNavbar(loggedOutNavbar);
+        setNavbar(loggedOutNavbar) : 
+        setNavbar(loggedInNavbar);
     }, [profile, setNavbar]
   )
 
@@ -47,7 +43,7 @@ function Navbar({profile, setProfile}){
       {
         navbar.map(([title, url]) => (
           // The hash is not a good fix for this.
-          <Link to={url} key={Math.random() * (2 << 64)} className="rounded-lg px-3 py-2 text-slate-700 font-medium hover:bg-slate-100 hover:text-slate-900" 
+          <Link to={url} key={title} className="rounded-lg px-3 py-2 text-slate-700 font-medium hover:bg-slate-100 hover:text-slate-900" 
             onClick={(e) => {
               if(title === 'Log out') {
                 logOut(e)
