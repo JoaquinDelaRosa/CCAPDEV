880b936f407ecd582e055ddb9764729002ed1601
JoaquinDelaRosa <joaquin_delarosa@dlsu.edu.ph>
2022-04-26 19:44:16 +0800

    fix: issue in navbar styling

--- src/pages/navbar.jsx
+++ src/pages/navbar.jsx
@@ -16,42 +16,45 @@ function Navbar({profile, setProfile}){
     ['Post', './postpage'],
     ['Feed', './feed'],
     ['Log in', './login'],
-    ['Sign Up', './registration'],
-    ['Log out', './']
+    ['Sign Up', './registration']
   ]);
 
   useEffect(() => {
     setNavbar(navbar)
   }, [navbar]);
   
-  function updateNavbar() {
-    profile === null ? 
-      setNavbar([ // if logged out
-        ['Home', './'],
-        ['Post', './postpage'],
-        ['Feed', './feed'],
-        ['Log in', './login'],
-        ['Sign Up', './registration']
-      ]) : 
-      setNavbar([ // if logged in
-        ['Home', './'],
-        ['Post', './postpage'],
-        ['Feed', './feed'],
-        ['Log out', './'],
-        ['Profile', './profile'],
-        ['Settings', './settings']
-      ]);
-  }
+  useEffect(() => {
+      profile === null ? 
+        setNavbar([ // if logged out
+          ['Home', './'],
+          ['Post', './postpage'],
+          ['Feed', './feed'],
+          ['Log in', './login'],
+          ['Sign Up', './registration']
+        ]) : 
+        setNavbar([ // if logged in
+          ['Home', './'],
+          ['Post', './postpage'],
+          ['Feed', './feed'],
+          ['Log out', './'],
+          ['Profile', './profile'],
+          ['Settings', './settings']
+        ]);
+    }, [profile, setNavbar]
+  )
 
   return (
     <nav className="flex flex-auto space-x-4">
       {console.log(profile) /* check if actually log out*/}
       {
         navbar.map(([title, url]) => (
-          <Link to={url} key={title} className="rounded-lg px-3 py-2 text-slate-700 font-medium hover:bg-slate-100 hover:text-slate-900" 
+          // The hash is not a good fix for this.
+          <Link to={url} key={Math.random() * (2 << 64)} className="rounded-lg px-3 py-2 text-slate-700 font-medium hover:bg-slate-100 hover:text-slate-900" 
             onClick={(e) => {
-              if(title === 'Log out') {logOut(e)};
-              updateNavbar();
+              if(title === 'Log out') {
+                logOut(e)
+                console.log(profile);
+              };
             }}>
             {title}
           </Link>
