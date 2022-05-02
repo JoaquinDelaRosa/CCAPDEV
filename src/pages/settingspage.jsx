9306ebbe6b079dfba46080af124539deb503c090
JoaquinDelaRosa <joaquin_delarosa@dlsu.edu.ph>
2022-05-02 20:43:31 +0800

    refactor: settings aligned with SOP

--- src/pages/settingspage.jsx
+++ src/pages/settingspage.jsx
@@ -14,8 +14,14 @@ function SettingsPage({profile, setProfile}){
     "dateJoined": new Date()
   });
 
+  const [oldPassword, setOldPasword] = useState("");
+  const [passwordEdited, setPasswordEdited] = useState(false);
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [confirmOldPassword, setConfirmOldPassword] = useState("");
+
   useEffect(() => {
     setProfile(profile);
+    setOldPasword(profile["password"]);
   }, [profile, setProfile]);
 
   useEffect(() => {
@@ -32,6 +38,10 @@ function SettingsPage({profile, setProfile}){
     setEditted( values => ({...values, [name] : value}))
   }
 
+  const canSubmit = () => {
+    return (confirmPassword === profile["password"]) && (oldPassword === confirmOldPassword)
+  }
+
   const handleSubmit = (event) => {
     event.preventDefault();
     Object.assign(profile, editted);
@@ -40,70 +50,129 @@ function SettingsPage({profile, setProfile}){
   }
 
   return (
-    <div className="h-screen bg-gray-400 p-5">
+    <div className="h-screen bg-gray-800 text-white p-5">
       <div id="edit section" className="mb-10">
-        <h1 className= "mb-2  text-left font-sans text-4xl font-bold w-80"> 
+        <h1 className= "mb-4  text-left font-sans text-4xl font-bold w-80"> 
           Edit your profile!
         </h1>
 
-        <form onSubmit= {handleSubmit} className="my-4">
-          <div className="flex my-3" id="username">
-            <label className="mr-3"> 
-              <h2 className="font-sans font-medium text-1xl"> Name: </h2> 
-            </label>
+        <form onSubmit= {handleSubmit} className="my-4 py-4 px-4">
+          <table className="table-auto">
+            <tr className="" id="username">
+              <td className="flex justify-items-center">
+                <label className="mr-3"> 
+                  <h2 className="font-sans font-medium text-2xl"> Name: </h2> 
+                </label>
+              </td>
 
-            <div> 
-              <input type="text"
-                className="form-input bg-gray-100 text-left font-sans font-light w-80 rounded-sm"
-                placeholder={editted["username"] === "" ? profile["username"] : editted["username"]}
-                onChange={(e) => {inputHandler("username", e.target.value)}}
-              />
-            </div>
-          </div>
+              <td> 
+                <input type="text"
+                  className="form-input bg-gray-100 text-left font-sans font-light w-80 rounded-sm text-black"
+                  placeholder={editted["username"] === "" ? profile["username"] : editted["username"]}
+                  onChange={(e) => {inputHandler("username", e.target.value)}}
+                />
+              </td>
+            </tr>
 
-          <div className="flex my-3" id="password">
-            <label className="mr-3"> 
-              <h2 className="font-sans font-medium text-1xl">
-                Password: 
-              </h2> 
-            </label>
+            <tr className="" id="password">
+              <td className="flex justify-items-center">
+                <label className="mr-3"> 
+                  <h2 className="font-sans font-medium text-2xl">
+                    Password: 
+                  </h2> 
+                </label>
+              </td>
 
-            <div> 
-              <input type="password"
-                className="form-input bg-gray-100 text-left font-sans font-light w-80 rounded-sm"
-                placeholder={"Change password"}
-                onChange={(e) => {inputHandler("password", e.target.value)}}
-              />
-            </div>
-          </div>
+              <td> 
+                <input type="password"
+                  className="form-input bg-gray-100 text-left font-sans font-light w-80 rounded-sm text-black"
+                  placeholder={"Change password"}
+                  onChange={(e) => {
+                    inputHandler("password", e.target.value);
+                      if(e.target.value === ""){
+                        setPasswordEdited(false);
+                      } else 
+                        setPasswordEdited(true);
+                    }
+                  }
+                />
+              </td>
+            </tr>
 
-          <div id="about-me" className="my-3">
-            <label className="mr-3"> 
-              <h2 className="font-sans font-medium text-1xl"> 
-              About Me: 
-              </h2> 
-            </label>
+            <tr className="" id="confirm-password" hidden={!passwordEdited}>
+              <td className="flex justify-items-center">
+                <label className="mr-3"> 
+                  <h2 className="font-sans font-medium text-2xl">
+                    Enter Old Password: 
+                  </h2> 
+                </label>
+              </td>
 
-            <div> 
-              <textarea
-                className="w-full max-w-xl h-fit mb-1 text-lg font-mono bg-slate-50 overflow-hidden resize-none"
-                placeholder={profile["about"]}
-                onChange={(e) => {
-                  inputHandler("about", e.target.value);
-                  e.target.style.height = 'inherit';
-                  e.target.style.height = `${e.target.scrollHeight}px`; 
-                }}
-              />
-            </div>
-          </div>
+              <td> 
+                <input type="password"
+                  className="form-input bg-gray-100 text-left font-sans font-light w-80 rounded-sm text-black"
+                  placeholder={"Change password"}
+                  onChange={(e) => {
+                      setOldPasword(e.target.value);
+                    }
+                  }
+                  required = {passwordEdited}
+                />
+              </td>
+            </tr>
+            
+            <tr className="" id="confirm-password" hidden={!passwordEdited}>
+              <td className="flex justify-items-center">
+                <label className="mr-3"> 
+                  <h2 className="font-sans font-medium text-2xl">
+                    Confirm New Password: 
+                  </h2> 
+                </label>
+              </td>
+
+              <td> 
+                <input type="password"
+                  className="form-input bg-gray-100 text-left font-sans font-light w-80 rounded-sm text-black"
+                  placeholder={"Change password"}
+                  onChange={(e) => {
+                      setConfirmPassword(e.target.value);
+                    }
+                  }
+                  required = {passwordEdited}
+                />
+              </td>
+            </tr>
+
+            <tr id="about-me" className="my-3">
+              <td>
+                <label className="mr-3"> 
+                  <h2 className="font-sans font-medium text-2xl"> 
+                  About Me: 
+                  </h2> 
+                </label>
+              </td>
+
+              <td> 
+                <textarea
+                  className="w-full max-w-3xl h-fit mb-1 text-lg font-mono bg-slate-50 overflow-hidden resize-none text-black"
+                  placeholder={profile["about"]}
+                  onChange={(e) => {
+                    inputHandler("about", e.target.value);
+                    e.target.style.height = 'inherit';
+                    e.target.style.height = `${e.target.scrollHeight}px`; 
+                  }}
+                />
+              </td>
+            </tr>
+          </table>
 
           <div id="submit">
             <input type="submit"
               value={"Save Changes"}
               className= {"py-1 px-8 rounded-full w-auto text-white bg-orange-500 hover:cursor-pointer hover:bg-orange-600"}
+              disabled = {canSubmit()}
             />
           </div>
-                
         </form>
       </div>
 
