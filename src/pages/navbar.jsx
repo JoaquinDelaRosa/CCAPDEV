3cdb5873f0e7044ce9310c923c9e6716ca5b71d8
JoaquinDelaRosa <80933658+JoaquinDelaRosa@users.noreply.github.com>
2022-05-03 21:36:26 +0800

    Revert "Begin navbar styling"

--- src/pages/navbar.jsx
+++ src/pages/navbar.jsx
@@ -15,7 +15,7 @@ const loggedInNavbar = [
   ['Feed', './feed'],
   ['Log out', './'],
   ['Profile', './profile'],
-  ["Danger Zone", "./dangerpage"]
+  ['Settings', './settings']
 ]
 
 function Navbar({profile, setProfile}){
@@ -40,12 +40,14 @@ function Navbar({profile, setProfile}){
 
   return (
     <nav className="sticky top-0 flex flex-auto space-x-4 py-2 pl-3 opacity-95 bg-gray-800 ring-1 ring-cyan-[#2d467d] outline-white bg-gradient-to-br from-gray-900 backdrop-blur-xl">
+      {console.log(profile) /* check if actually log out*/}
       {
         navbar.map(([title, url]) => (
           <Link to={url} key={title} className="px-4 py-1 rounded-lg font-medium hover:bg-gray-900 hover:ring-1 hover:ring-orange-500 hover:text-orange-100 text-wText" 
             onClick={(e) => {
               if(title === 'Log out') {
-                logOut(e);
+                logOut(e)
+                console.log(profile);
               };
             }}>
             {title}
