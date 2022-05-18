15aa9975fad38832e789cbd60f70652d8e70e1cc
JoaquinDelaRosa <joaquin_delarosa@dlsu.edu.ph>
2022-05-18 09:09:57 +0800

    add: profile API

---
+++ api/routes/profile.js
@@ -0,0 +1,61 @@
+var express = require('express');
+var router = express.Router();
+var bodyParser = require('body-parser');
+const app = require('../app');
+
+let profiles = [{
+  "id": 1,
+  "pfp": "None",  // Handle images
+  "name": "Andrei",
+  "username": "AChua123",
+  "password": "12345",
+  "about": "A person lurking in this website (not a bot)",
+  "gender": "Male",
+  "saves": [],
+  "posts": [],
+  "dateJoined": "March 20, 2022"
+}]
+
+// TOOO: Replacre with Mongoose query
+function getProfile(queryUsername){
+  let ret = {}
+  console.log(queryUsername);
+  profiles.forEach((value) => {
+    if (value.username === queryUsername){
+      ret = value;
+    }
+  })
+
+  return ret;
+}
+
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  const profile = getProfile(req.query.username);
+  res.send(profile);
+});
+
+/* POST users listing */ 
+router.post('/', function(req, res, next) {
+  // TODO: Replace with Mongoose
+  profiles.push(req.body);
+  res.send("Successfully added profile")
+})
+
+/* PATCH users listing */
+router.patch('/', function(req, res, next) {
+  // TODO: Replace with Mongoose
+  // This is incomplete
+  
+  res.send("Successfully editted profile")
+})
+
+/* DELETE users listing */
+router.delete('/', function(req, res, next) {
+  // TODO: Replace with Mongoose
+  profiles = profiles.filter((value) => {req.query.username !== value.id});
+  res.send("Successfully deleted profile")
+})
+
+module.exports = router;
