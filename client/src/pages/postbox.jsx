d366573284f30f67eb65755c7e5bc68131ce9190
JoaquinDelaRosa <joaquin_delarosa@dlsu.edu.ph>
2022-06-07 11:03:42 +0800

    add: upvote / downvote / fav in postbox

--- client/src/pages/postbox.jsx
+++ client/src/pages/postbox.jsx
@@ -21,7 +21,10 @@ const defaultPost = {
 }
 
 function Postbox({ content }) {
-    const [post, setPost] = useState(defaultPost);
+    const [post, setPost] = useState(defaultPost);  
+    const [upvoted, setUpvoted] = useState(false);
+    const [downvoted, setDownVoted] = useState(false);
+    const [favorite, setFavorited] = useState(false);
 
     useEffect(() => {
         if (content !== undefined) {
@@ -30,6 +33,54 @@ function Postbox({ content }) {
           setPost(defaultPost);
         }
     }, [content]);
+
+    
+  const handleUpvote = () => {
+    if (upvoted)
+      setPost( values => ({...values, "upvotes" : post.upvotes - 1}))
+    else {
+      setPost( values => ({...values, "upvotes" : post.upvotes + 1}))
+      if (downvoted)
+        setPost( values => ({...values, "downvotes" : post.downvotes -1}))
+    }
+
+    setDownVoted(false);
+    setUpvoted(!upvoted);
+  }
+
+  const handleDownvotes = () => {
+    if (downvoted)
+      setPost( values => ({...values, "downvotes" : post.downvotes - 1}))
+    else {
+      setPost( values => ({...values, "downvotes" : post.downvotes + 1}))
+      if (upvoted)
+        setPost( values => ({...values, "upvotes" : post.upvotes -1}))
+    }
+
+    setUpvoted(false);
+    setDownVoted(!downvoted);
+  }
+
+  
+  const handleFavorites = () => {
+    let profile = {saves : []};
+    // FETCH associated profile data
+
+    if (!favorite && !profile.saves.includes(post.id)){
+      profile.saves.push(post.id);
+    } else if (favorite && profile.saves.includes(post.id)){
+      profile.saves.splice(profile.saves.indexOf(post.id));
+    }
+
+    if (favorite)
+      setPost( values => ({...values, "favorites" : post.favorites - 1}));
+    else 
+      setPost( values => ({...values, "favorites" : post.favorites + 1}));
+
+    // PATCH profile setProfile( values => ({...values, "saves" : profile.saves}));
+    
+    setFavorited(!favorite);
+  }
 
     return (
       <div className= "w-full h-auto border-gray-800 bg-gray-700 hover:bg-gray-500 px-4 py-2 border-2 mb-3">
@@ -59,7 +110,7 @@ function Postbox({ content }) {
             <input type="button" 
               className={`w-fit mr-5 hover:cursor-pointer text-xl text-green-700 hover:text-green-500
               ${false ? "font-extrabold" : "font-bold"}`}
-              onClick={(e) => {; e.target.value = post.upvotes}}
+              onClick={(e) => {handleUpvote(); e.target.value = post.upvotes}}
               value={post.upvotes}
             />
           </div>
@@ -68,7 +119,7 @@ function Postbox({ content }) {
             <input type="button" 
               className={`w-fit mr-5 hover:cursor-pointer text-xl text-red-700 hover:text-red-500
               ${false ? "font-extrabold" : "font-bold"}`}
-              onClick={(e) => {; e.target.value = post.downvotes}}
+              onClick={(e) => {handleDownvotes(); e.target.value = post.downvotes}}
               value={post.downvotes}
             />
           </div>
@@ -77,7 +128,7 @@ function Postbox({ content }) {
             <input type="button" 
               className={`w-fit mr-5 hover:cursor-pointer text-xl text-yellow-700 hover:text-yellow-500
               ${false ? "font-extrabold" : "font-bold"}`}
-              onClick={(e) => {; e.target.value = post.favorites}}
+              onClick={(e) => {handleFavorites(); e.target.value = post.favorites}}
               value={"☆ " +  post.favorites}
             />
           </div>
