16792c810c1289103715a9be25cc0b2e1426d82e
JoaquinDelaRosa <joaquin_delarosa@dlsu.edu.ph>
2022-06-21 19:39:10 +0800

    fix: should render author username not id

--- client/src/pages/author.jsx
+++ client/src/pages/author.jsx
@@ -1,13 +1,46 @@
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 
+const userURL = '/api/user';
+
 function Author({context, author}) {
+    const [username, setUsername] = useState("");
+    useEffect(() => {
+        if (author != ""){
+            let data = fetch(userURL + "?id=" + author, { 
+                method : "GET",
+                headers : {
+                  'Content-type': 'application/json'
+                },
+              })
+              .then((response) => {
+                return response.json();
+              })
+              .catch((error) => {
+                console.log("Error in retrieving profile information" );
+              });
+        
+              if (data) {
+                data.then( (profileData) => {
+                  if (profileData) {
+                    setUsername(profileData.username);
+                  } else{
+                    setUsername("");
+                  }
+                });
+              }
+        }
+    }, [author])
+
+
     return (
-    <span className={(author.length > 0 ? "hover:text-blue-300" : "")}>
-        { author.length > 0  && 
+    <span className={(username.length > 0 ? "hover:text-blue-300" : "")}>
+        { username.length > 0  && 
         <Link to={"../profile?id=" + author}>
-            {(author.length > 0) ? author : "<Anonymous>"}
+            {username}
         </Link>
         }
 
