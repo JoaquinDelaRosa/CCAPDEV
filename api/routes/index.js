358812c282d6dbff696a52411ceb76c466f970e3
JoaquinDelaRosa <joaquin_delarosa@dlsu.edu.ph>
2022-06-27 06:51:57 +0800

    heroku: fixn

--- api/routes/index.js
+++ api/routes/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 /* GET post listing. */
 // Query params: id
 router.get('/', async function(req, res, next) {
-    res.sendFile("../client/public/index.html");
+    res.sendFile("../client/src/index.js");
 });
 
 module.exports = router;
