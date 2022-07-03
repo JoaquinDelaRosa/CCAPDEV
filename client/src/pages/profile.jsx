f67b39f0e823e42f6910aebd2b8ef4a868d4ec05
JoaquinDelaRosa <joaquin_delarosa@dlsu.edu.ph>
2022-07-03 08:02:05 +0800

    heroku deploy

--- client/src/pages/profile.jsx
+++ client/src/pages/profile.jsx
@@ -6,8 +6,6 @@ import UserPosts from "./userposts";
 import UserSaves from "./usersaves";
 import Cookies from "js-cookie";
 
-const anonymous = require("../images/anon.webp")
-
 // TO-DO:   Posts should be fetched rather than hardcoded.
 
 const userURL = '/api/user';
@@ -58,7 +56,7 @@ function Profile({context, setContext}){
         <div className="justify-center flex pt-3 pb-1" id="pfp-section">  
           <span className="mt-3 w-32 h-32">
             <img 
-                src={profile["pfp"] !==null ? profile["pfp"] : anonymous} 
+                src={profile["pfp"] !==null ? profile["pfp"] : require("../images/anon.png")} 
                 alt={profile["username"] + "'s profile picture"}
                 className = "w-full h-full object-cover rounded-full overflow-hidden hover:opacity-80 hover:cursor-pointer"
             />
